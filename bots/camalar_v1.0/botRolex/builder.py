7be1b605aa73aab1470b7ded52265e1faa7ce947
Pablo Valentino Chaume Magraner <PVCHAMAG@upv.edu.es>
2026-03-17 13:17:29 +0100

    camalar_v1.1

--- bots/camalar_v1.0/botRolex/builder.py
+++ bots/camalar_v1.0/botRolex/builder.py
@@ -1,37 +1,36 @@
 from operator import pos
 
 from cambc import Controller, Direction, EntityType, Environment, Position
-import bugnav
-import random
+import math
 
 def run_builder(self, c: Controller):
     #logica del builder aqui
-    if(self.conveyor_mode): conveyorHome(self, c)
     oreCerca(self, c)
     current = c.get_position()
     target = None
     if(len(self.objetivos) > 0):
         target = self.objetivos[0]
 
+    if target is not None: c.draw_indicator_line(current, target, 204, 39, 245)
     if  (target is not None):
-        siguiente_dir = self.navegador.moveTo(c, target, four_dirs=False)
+        siguiente_dir = self.navegador.moveTo(c, target, four_dirs=True)
         move_pos = current.add(siguiente_dir)
         c.draw_indicator_line(current, move_pos, 66, 245, 39)
-        if c.can_build_harvester(move_pos):
-            c.build_harvester(move_pos)
-            self.conveyor_mode = True
-        else:
-            if c.can_build_road(move_pos):
-                c.build_road(move_pos)
+        if c.can_build_harvester(target):
+            c.build_harvester(target)
+            self.objetivos.remove(target)
+        elif(math.sqrt(current.distance_squared(target)) >= math.sqrt(2)):
+            if c.can_build_conveyor(move_pos, siguiente_dir.opposite()):
+                c.build_conveyor(move_pos, siguiente_dir.opposite())
             if c.can_move(siguiente_dir):
                 c.move(siguiente_dir)
         
     else:
-        move_dir = self.navegador.moveDvD(c)
+        move_dir = self.navegador.moveDvD(c, four_dirs=True)
         move_pos = current.add(move_dir)
         # we need to place a conveyor or road to stand on, before we can move onto a tile
-        if c.can_build_road(move_pos):
-            c.build_road(move_pos)
+        if c.can_build_conveyor(move_pos, move_dir.opposite()):
+            c.build_conveyor(move_pos, move_dir.opposite())
         if c.can_move(move_dir):
             c.move(move_dir)
     pass
@@ -67,6 +66,8 @@ def oreCerca(self, c: Controller):
     for tile in lista:
         if c.get_tile_env(tile) in (Environment.ORE_TITANIUM, Environment.ORE_AXIONITE) and c.get_tile_building_id(tile) is None:
             self.objetivos.append(tile)
+        elif tile in self.objetivos:
+            self.objetivos.remove(tile)
     current = c.get_position()
     self.objetivos.sort(key=lambda p: current.distance_squared(p))
     pass
--- bots/camalar_v1.0/bugnav.py
+++ bots/camalar_v1.0/bugnav.py
@@ -11,12 +11,20 @@ class BugNav:
     start = None
     prevGoal = None
 
+    def reset(self):
+        self.followingWall = False
+        self.hitPoint = None
+        self.wallDir = None
+        self.visitedStates.clear()
+
     def moveTo(self, c: Controller, goal: Position, four_dirs: bool):
         current = c.get_position()
 
         if(goal != self.prevGoal):
+            self.reset()
             self.start = current
             self.prevGoal = goal
+
 
         if(not self.followingWall):
             nextDir = current.direction_to(goal)
@@ -41,17 +49,19 @@ class BugNav:
             self.wallDir = nextDir
 
             self.visitedStates.clear()
-         
+        
+        c.draw_indicator_dot(current, 245, 63, 39)
         # Detectar Bucle
-        stateKey = (current.x, current.y, self.wallDir)
-        if(stateKey in self.visitedStates):
+        #stateKey = (current.x, current.y, self.wallDir)
+        #if(stateKey in self.visitedStates):
             # Process Giving UP
-            return Direction.CENTRE
-        self.visitedStates.add(stateKey)
-
+         #   return Direction.CENTRE
+        #self.visitedStates.add(stateKey)
+        
+        self.wallDir = current.direction_to(goal)
         nextDir = self.followWall(c, four_dirs=four_dirs)
 
-        if(nextDir is Direction.CENTRE):
+        if(nextDir == Direction.CENTRE):
             # Process Giving UP
             return Direction.CENTRE
         
@@ -60,7 +70,7 @@ class BugNav:
         # Leave obstacle if back on M-line closer to goal
         # !!! Add seansing condition: If I see goal and no obstacle leave followingWall !!!
         nextPos = current.add(nextDir)
-        if((self.onMline(nextPos) and nextPos.distance_squared(goal) < self.hitPoint.distance_squared(goal)) or nextPos.distance_squared(goal) < 4):
+        if((self.onMline(nextPos, c) and nextPos.distance_squared(goal) < self.hitPoint.distance_squared(goal))):
             self.followingWall = False
             self.visitedStates.clear()
 
@@ -69,36 +79,40 @@ class BugNav:
     def followWall(self, c: Controller, four_dirs: bool):
         dir = self.wallDir
         current = c.get_position()
-        for i in range(9):
+        for i in range(8):
             dir = dir.rotate_left()
 
             dx, dy = dir.delta()
             if(four_dirs and dx != 0 and dy != 0):
                 continue
 
-            if(c.can_move(self.wallDir) or c.can_build_road(current.add(self.wallDir))):
+            if(c.can_move(dir) or c.can_build_road(current.add(dir))):
                 self.wallDir = dir
                 return dir
 
         return Direction.CENTRE
     
-    def onMline(self, p: Position):
+    def onMline(self, p: Position, c: Controller):
         d1 = math.sqrt(self.start.distance_squared(p))
         d2 = math.sqrt(p.distance_squared(self.prevGoal))
         d3 = math.sqrt(self.start.distance_squared(self.prevGoal))
 
+        c.draw_indicator_line(self.start, self.prevGoal, 228, 245, 39)
+
         return abs((d1 + d2) - d3) < 0.5
     
     dvd = None
+    fdirs = [Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST]
     dirs = [Direction.NORTH, Direction.NORTHEAST, Direction.NORTHWEST, Direction.WEST, Direction.EAST, Direction.SOUTHEAST, Direction.SOUTH, Direction.SOUTHWEST]
-    def moveDvD(self, c: Controller):
+    def moveDvD(self, c: Controller, four_dirs: bool):
         current = c.get_position()
         if(self.dvd is None):
-            self.dvd = random.choice(self.dirs)
+            self.dvd = random.choice(self.fdirs) if four_dirs else random.choice(self.dirs)
         
         if(c.can_move(self.dvd) or c.can_build_road(current.add(self.dvd))):
             return self.dvd
         
-        self.dvd = random.choice(self.dirs)
+        self.dvd = random.choice(self.fdirs) if four_dirs else random.choice(self.dirs)
         return self.dvd
+
 
